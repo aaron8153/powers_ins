635cc2990f26aae684e1d4e187302f13b47668c8
Aaron Jacobs <aaron.jacobs@jbasol.com>
2010-12-20 13:52:23 -0600

    comments css separator shit done

--- public/javascripts/fb-comments.js
+++ public/javascripts/fb-comments.js
@@ -1,4 +1,6 @@
-jQuery('.viewcomments').click(function() {	
+jQuery('.viewcomments').click(function() {
+	jQuery('.viewcomments span').toggle();
+	jQuery('.about-box-separator').toggle();
 	jQuery('.comments .fb_iframe_widget').toggle('fast', function(){
 		jQuery('.floater').stickyfloat({
 			duration: 200
